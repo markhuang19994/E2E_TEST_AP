351b57d1535dcdd94d9ea61daec565871561edc2
andychen <andy.chen@iisigroup.com>
2018-03-13 16:02:29 +0800

    add page uni test

--- ete-selenium/src/main/java/com/driver/WebDriverUtil.java
+++ ete-selenium/src/main/java/com/driver/WebDriverUtil.java
@@ -2,11 +2,15 @@ package com.driver;
 
 import com.google.common.base.Predicate;
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
@@ -14,6 +18,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 /**
  * @author MarkHuang
@@ -82,5 +88,56 @@ public class WebDriverUtil {
             e.printStackTrace();
         }
     }
+
+    /**
+     * for JUnit test
+     * @return
+     */
+    public static WebDriver getWebDriver(){
+
+        String driverPath = "src/test/resources/webdriver/chromedriver";
+        String driverOption = "--disable-gpu";
+        String driver = "chrome";
+        int windowWidth = 3840;
+        int windowHeight = 2160;
+        int positionX = 5;
+        int positionY = 0;
+        int driverWait = 10;
+
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            System.setProperty("webdriver.chrome.driver", driverPath);
+        } else {
+            System.setProperty("webdriver.chrome.com.driver", driverPath + ".exe");
+        }
+        WebDriver webDriver;
+        ChromeOptions chromeOptions = new ChromeOptions();
+        for (String option : driverOption.split("\\|")) {
+            chromeOptions.addArguments(option);
+        }
+        DesiredCapabilities caps = DesiredCapabilities.chrome();
+        LoggingPreferences logPrefs = new LoggingPreferences();
+        logPrefs.enable(LogType.BROWSER, Level.INFO);
+        caps.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
+        caps.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        switch (driver.toLowerCase()) {
+            case "chrome":
+                webDriver = new ChromeDriver(caps);
+                break;
+            case "firefox":
+                webDriver = new FirefoxDriver();
+                break;
+            case "ie":
+                webDriver = new InternetExplorerDriver();
+                break;
+            default:
+                webDriver = new ChromeDriver(chromeOptions);
+        }
+        webDriver.manage().window().setSize(new Dimension(windowWidth, windowHeight));
+        webDriver.manage().window().setPosition(new Point(positionX, positionY));
+        webDriver.manage().timeouts().implicitlyWait(driverWait, TimeUnit.SECONDS);
+        return webDriver;
+    }
+
+
 
 }
---
+++ ete-selenium/src/test/java/com/project/pcl2/PageStep1Test.java
@@ -0,0 +1,69 @@
+package com.project.pcl2;
+
+import com.driver.WebDriverUtil;
+import com.model.PageData;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by AndyChen on 2018/3/8.
+ */
+public class PageStep1Test {
+
+    PageStep1 step1;
+
+    WebDriver driver;
+
+    PageData pageData;
+
+
+
+    @Before
+    public void setUp() throws Exception {
+
+        driver = WebDriverUtil.getWebDriver();
+        String jsonStr = "{\n" +
+                "    \"datas\": [{\n" +
+                "        \"id\": \"ino\",\n" +
+                "        \"value\": \"M123456789\",\n" +
+                "        \"dataType\": \"text\",\n" +
+                "        \"order\": \"1\",\n" +
+                "        \"beforeScript\" : \"\"\n" +
+                "    }, {\n" +
+                "        \"id\": \"cno\",\n" +
+                "        \"value\": \"4563180400000001\",\n" +
+                "        \"dataType\": \"text\",\n" +
+                "        \"order\": \"\",\n" +
+                "        \"beforeScript\" : \"\"\n" +
+                "    },{\n" +
+                "        \"id\": \"test\",\n" +
+                "        \"value\": \"4563180400000001\",\n" +
+                "        \"dataType\": \"text\",\n" +
+                "        \"order\": \"3\",\n" +
+                "        \"beforeScript\" : \"\"\n" +
+                "    }]\n" +
+                "}";
+        pageData = new PageData();
+        pageData.setPageUrl("http://localhost:8081/extfunc02/page/index");
+        pageData.setDataJsonStr(jsonStr);
+
+        step1 = new PageStep1(driver, pageData);
+        step1.testPage();
+
+    }
+
+    @After
+    public void tearDown(){
+        // nothing to clenaup
+    }
+
+    @Test
+    public void set_delete_day_then_clean_file() throws Exception {
+    }
+
+
+
+
+}
