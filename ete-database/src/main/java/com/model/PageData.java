a2edd04535646666c5479dbfac0ce44aa3fa6d3b
andychen <andy.chen@iisigroup.com>
2018-03-13 16:06:52 +0800

    modify pageData impl

--- ete-database/src/main/java/com/model/PageData.java
+++ ete-database/src/main/java/com/model/PageData.java
@@ -1,8 +1,13 @@
 package com.model;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.util.DataUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author AndyChen
@@ -13,23 +18,13 @@ import java.util.List;
  */
 public class PageData implements Serializable {
 
+    private static final Logger logger = LoggerFactory.getLogger(PageData.class);
+
     private String pageUrl;
 
     private String dataJsonStr;
-    //TODO json format
 
-    /**
-     * {
-     *  eleId : "",
-     *  eleValue:"",
-     *  type,
-     *  order
-     *
-     * }
-     *
-     */
-
-    private ArrayList<JsonData> jsonDatas; //TODO must order data
+    private ArrayList<JsonData> jsonDatas; //TODOed must order data
 
 
     public String getPageUrl() {
@@ -46,6 +41,35 @@ public class PageData implements Serializable {
 
     public void setDataJsonStr(String dataJsonStr) {
         this.dataJsonStr = dataJsonStr;
+        JsonDatas datas = null;
+        //TODO 解析
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+//            List<JsonData> jsonDatas = DataUtil.parseJsonToObjectList(dataJsonStr, JsonData.class);
+            datas = mapper.readValue(dataJsonStr, JsonDatas.class);
+            List<JsonData> jsonDatas = datas.getDatas();
+            Collections.sort(jsonDatas, new Comparator<JsonData>() {
+                @Override
+                public int compare(JsonData o1, JsonData o2) {
+                    int oint1 = -1;
+                    int oint2 = -1;
+                    try {
+                        oint1 = Integer.parseInt(o1.getOrder());
+                        oint2 = Integer.parseInt(o2.getOrder());
+                    } catch (NumberFormatException e) {
+                        if(oint1 == -1)
+                            return -1;
+                        return 1;
+                    }
+                    return oint1 - oint2;
+                }
+            });
+            ArrayList<JsonData> resultDatas = new ArrayList<>();
+            resultDatas.addAll(jsonDatas);
+            this.setJsonDatas(resultDatas);
+        } catch (IOException e) {
+            logger.debug("");
+        }
     }
 
     public ArrayList<JsonData> getJsonDatas() {
@@ -55,4 +79,18 @@ public class PageData implements Serializable {
     private void setJsonDatas(ArrayList<JsonData> jsonDatas) {
         this.jsonDatas = jsonDatas;
     }
+
+    private static class JsonDatas {
+
+        private List<JsonData> datas;
+
+        public List<JsonData> getDatas() {
+            return datas;
+        }
+
+        public void setDatas(List<JsonData> datas) {
+            this.datas = datas;
+        }
+    }
+
 }
