317e5523bc5a0deb58192ca150defd224248b972
andychen <andy.chen@iisigroup.com>
2018-03-15 11:23:52 +0800

    modify pageData model jsonStr parse

--- ete-database/src/main/java/com/model/PageData.java
+++ ete-database/src/main/java/com/model/PageData.java
@@ -1,5 +1,6 @@
 package com.model;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.util.DataUtil;
 import org.slf4j.Logger;
@@ -12,8 +13,8 @@ import java.util.*;
 /**
  * @author AndyChen
  * @version <ul>
- * <li>2018/3/7 AndyChen,new
- * </ul>
+ *          <li>2018/3/7 AndyChen,new
+ *          </ul>
  * @since 2018/3/7
  */
 public class PageData implements Serializable {
@@ -39,37 +40,14 @@ public class PageData implements Serializable {
         return dataJsonStr;
     }
 
-    public void setDataJsonStr(String dataJsonStr) {
+    public void setDataJsonStr(String dataJsonStr) throws IOException {
         this.dataJsonStr = dataJsonStr;
-        JsonDatas datas = null;
-        //TODO 解析
+        //TODOed 解析
         ObjectMapper mapper = new ObjectMapper();
-        try {
-//            List<JsonData> jsonDatas = DataUtil.parseJsonToObjectList(dataJsonStr, JsonData.class);
-            datas = mapper.readValue(dataJsonStr, JsonDatas.class);
-            List<JsonData> jsonDatas = datas.getDatas();
-            Collections.sort(jsonDatas, new Comparator<JsonData>() {
-                @Override
-                public int compare(JsonData o1, JsonData o2) {
-                    int oint1 = -1;
-                    int oint2 = -1;
-                    try {
-                        oint1 = Integer.parseInt(o1.getOrder());
-                        oint2 = Integer.parseInt(o2.getOrder());
-                    } catch (NumberFormatException e) {
-                        if(oint1 == -1)
-                            return -1;
-                        return 1;
-                    }
-                    return oint1 - oint2;
-                }
-            });
-            ArrayList<JsonData> resultDatas = new ArrayList<>();
-            resultDatas.addAll(jsonDatas);
-            this.setJsonDatas(resultDatas);
-        } catch (IOException e) {
-            logger.debug("");
-        }
+        List<JsonData> jsonDatas = mapper.readValue(dataJsonStr, new TypeReference<List<JsonData>>() {});
+        ArrayList<JsonData> resultDatas = new ArrayList<>();
+        resultDatas.addAll(jsonDatas);
+        this.setJsonDatas(resultDatas);
     }
 
     public ArrayList<JsonData> getJsonDatas() {
@@ -78,19 +56,6 @@ public class PageData implements Serializable {
 
     private void setJsonDatas(ArrayList<JsonData> jsonDatas) {
         this.jsonDatas = jsonDatas;
-    }
-
-    private static class JsonDatas {
-
-        private List<JsonData> datas;
-
-        public List<JsonData> getDatas() {
-            return datas;
-        }
-
-        public void setDatas(List<JsonData> datas) {
-            this.datas = datas;
-        }
     }
 
 }
