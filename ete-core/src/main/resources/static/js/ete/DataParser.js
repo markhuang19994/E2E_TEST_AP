337d71cef7d7436ea3adf22e454b948803bdab74
markhuang1994 <markhuang1994@gmail.com>
2018-03-28 15:16:21 +0800

    1.前端js新增註解 2.新增gzip參數,假使容器換成tomcat即可啟用

--- ete-core/src/main/resources/static/js/ete/DataParser.js
+++ ete-core/src/main/resources/static/js/ete/DataParser.js
@@ -3,27 +3,33 @@
  * @since  2018/3/08
  */
 (function ($) {
+    //所有table element 最外層的div
     const $appendTable = $('#append_table');
     const jsonObjectKey = ['id', 'value', 'dataType', 'beforeScript'];
-    let testCase = {testCaseName: '', projectName: '', pageDatas: []};
     let testCaseName = '';
-    // let projectName = '';
-    console.log(projectName)
     let pageUrl = [];
-    //紀錄page的data table 物件
+    //紀錄每一頁的table element
     let $dataTable = [];
     //紀錄page中table的行數
     let countDataTableRow = [];
+    //有幾頁,所謂的page這邊不是真的分頁,只是複數table的隱藏與顯示,且一個table對應一個page,一次只顯示一個table
     let pageCount = 0;
     let nowDisplayPage = 0;
 
+    /**
+     * 在頁面載入時初始化頁面
+     */
     (function initPage() {
         newPage(true);
         displayPage(pageCount);
         $('.page_btn').eq(nowDisplayPage - 1).addClass('page-active');
     }());
 
-    //page is begin at 1,2,3....
+    /**
+     * 新增一個page,並隱藏其他page的table
+     * @param initFirst 初始化每一個table的第一行
+     * @param isAfter   頁面試添加在當前table之後還是所有table之後
+     */
     function newPage(initFirst = false, isAfter = false) {
         let pageIndex = 0;
         if (isAfter) {
@@ -46,6 +52,10 @@
             </li>`);
     }
 
+    /**
+     * 顯示傳入的page
+     * @param page
+     */
     function displayPage(page) {
         $(`table`).addClass('hide');
         $(`.page_tab`).eq(page - 1).removeClass('hide');
@@ -54,19 +64,29 @@
         nowDisplayPage = page;
     }
 
+    /**
+     * 刪除目前所有的page,並初始化所有的參數
+     */
     function deleteAllPage() {
         $('table').remove();
         $('.page_btn').remove();
         $dataTable = [];
+        pageUrl = [];
         countDataTableRow = [];
         pageCount = 0;
         nowDisplayPage = 0;
     }
 
+    /**
+     * 刪除頁面 下方所有page編號的hover效果
+     */
     function clearAllPageAction() {
         $('.page_btn').removeClass('page-active');
     }
 
+    /**
+     * 重置所有的page,table與page的對應關係完全刷新
+     */
     function resetAllPage() {
         $dataTable = [];
         countDataTableRow = [];
@@ -83,6 +103,9 @@
         });
     }
 
+    /**
+     * 重置table中所有的列的編號
+     */
     function resetTableRowOrder() {
         countDataTableRow[nowDisplayPage - 1] = 0;
         $dataTable[nowDisplayPage - 1].find('tr').each((index, me) => {
@@ -91,6 +114,10 @@
         });
     }
 
+    /**
+     * table顯示p元素還是input元素
+     * @param isWithInput 是否顯示input元素
+     */
     function displayTableSpan(isWithInput) {
         const tbody = $('tbody');
         if (isWithInput) {
@@ -102,13 +129,19 @@
         }
     }
 
+    /**
+     * 清空所有table的所有列的focus效果
+     */
     function clearPageFocusRow() {
         if ((!window.event.ctrlKey && !window.event.shiftKey)) {
             $(`.page_tab`).eq(nowDisplayPage - 1).find('tr').removeClass('focusNow');
         }
     }
 
-    function plusRow(e) {
+    /**
+     * 新增一列在當前focus的元素下方,若無元素被focus則新增一列在最下方
+     */
+    function plusRow() {
         let $tr = $dataTable[nowDisplayPage - 1].find('tr');
         let isAnyTrAdd = false;
         if ($tr.length !== 0) {
@@ -124,18 +157,21 @@
         } else {
             $dataTable[nowDisplayPage - 1].append(createTableDataElement(nowDisplayPage))
         }
-        resetTableRowOrder(e);
+        resetTableRowOrder();
     }
-
+    
     function plusPage() {
         newPage(true);
     }
-
+    
     function plusPageAfterCurrentPage() {
         newPage(true, true);
     }
 
-    function minusRow(e) {
+    /**
+     * 刪除當前focus的列元素,若無元素被focus則刪除最下方一列,若只剩一列則刪除當前頁面
+     */
+    function minusRow() {
         let $tr = $dataTable[nowDisplayPage - 1].find('tr');
         if ($tr.length === 1) {
             minusCurrentPage();
@@ -151,9 +187,12 @@
         if (!isAnyTrRemove) {
             $tr.last().remove();
         }
-        resetTableRowOrder(e);
+        resetTableRowOrder();
     }
 
+    /**
+     * 刪除最後面一頁,若只剩一頁則不做動作
+     */
     function minusPage() {
         if (pageCount <= 1) return;
         let nowPageIndex = nowDisplayPage - 1;
@@ -163,6 +202,9 @@
         if (nowPageIndex === pageCount) displayPage(pageCount);
     }
 
+    /**
+     * 刪除當前頁面,若只剩一頁則不動作
+     */
     function minusCurrentPage() {
         if (pageCount <= 1) return;
         let nowPageIndex = nowDisplayPage - 1;
@@ -172,19 +214,26 @@
         displayPage(nowPageIndex === 0 ? 1 : nowPageIndex);
     }
 
+    /**
+     * 動畫彈出popup
+     * @param popHtml popup的內容
+     */
     function popup(popHtml = '') {
         $('.modal-body').html(popHtml);
         $overlay.addClass('state-show');
         $modal.removeClass('state-leave').addClass('state-appear');
     }
 
-    function popupModelItem() {
+    /**
+     * 產生popup視窗,給使用者完善test case資料
+     */
+    function popupTestCaseInformation() {
         let urlElement = '';
         for (let i = 0; i < pageCount; i++) {
             urlElement += `
                     <div   class="pop-block-div">
                         <div><span>page ${i + 1} url : </span></div>
-                        <input class="pop-inp" type='text' />
+                        <input class="pop-inp pop-url" type='text' />
                     </div>`;
         }
         let popHtml = `
@@ -192,15 +241,18 @@
                 <h3>${projectName}</h3>
                 <div class="pop-block-div"   style='text-align: left;font-size: 1.2rem'>
                     <div><span>Test Case Name : </span></div>
-                    <input class="pop-inp" type='text' />
+                    <input class="pop-inp" id="pop-test-case-name" type='text' />
                 </div>
-                ${urlElement}
+                <div class="popup-inner-div">${urlElement}</div>
                 <div class="text-right" ><button>save</button></div>
             </div>`;
         popup(popHtml);
     }
 
-
+    /**
+     * 產生選擇test case的popup是視窗
+     * @param testCaseNameArray 所有test case的名稱
+     */
     function popupChoseTestCase(testCaseNameArray = []) {
         let testCaseElements = '';
         testCaseNameArray.forEach(name => {
@@ -219,10 +271,13 @@
         popup(popHtml);
     }
 
-
+    /**
+     * 解析json陣列,並設置test case的url
+     * @param jsonTestCaseArray test case的json陣列
+     * @returns {Promise.<void>}
+     */
     async function analyzeJsonArray(jsonTestCaseArray) {
-        testCase.testCaseName = jsonTestCaseArray[0]['testCaseName'] || '';
-        testCase.projectName = jsonTestCaseArray[0]['projectName'] || '';
+        testCaseName = jsonTestCaseArray[0]['testCaseName'] || '';
         jsonTestCaseArray.forEach(async data => {
             pageUrl.push(data['pageUrl']);
             await injectPageJsonData(JSON.parse(data['dataJsonStr']));
@@ -230,8 +285,8 @@
     }
 
     /**
-     * Inject json data in table
-     * @param pageJsonDataArray json data array
+     * 讀取json data並依照資料內容產生page與table
+     * @param pageJsonDataArray json data陣列
      */
     async function injectPageJsonData(pageJsonDataArray = {}) {
         return new Promise((resolve, reject) => {
@@ -251,6 +306,10 @@
         });
     }
 
+    /**
+     * 創建 table element
+     * @returns {string}
+     */
     function createTableElement() {
         pageCount++;
         return `
@@ -275,8 +334,8 @@
 
 
     /**
-     * Create table data element  and set select and input name and id by row
-     * @param page current page
+     * 創建table element的內部元素
+     * @param page current page 當前頁面
      * @returns {string} new table element string
      */
     function createTableDataElement(page) {
@@ -346,10 +405,11 @@
     }
 
     function generateJsonTestCase() {
-        testCase.testCaseName = testCaseName;
-        testCase.projectName = projectName;
-        testCase.pageDatas = generateJsonPageDataArray();
-        return testCase;
+        return {
+            testCaseName: testCaseName,
+            projectName: projectName,
+            pageDatas: generateJsonPageDataArray()
+        };
     }
 
     async function getJsonObjectArray() {
@@ -474,22 +534,39 @@
     //page event write under here
     //create new table element string and append in table
     $(document).on('click', '.plus', (e) => {
-        e.ctrlKey ? plusPage(e) : e.shiftKey ? plusPageAfterCurrentPage(e) : plusRow(e);
+        e.ctrlKey ? plusPage(e) : e.shiftKey ? plusPageAfterCurrentPage(e) : plusRow();
         // !(e.ctrlKey && !(plusPage(e)&&false)) && !(e.shiftKey && !(plusPageAfterCurrentPage(e)&&false)) && !(plusRow(e));
     });
 
     $(document).on('click', '.minus', (e) => {
-        e.ctrlKey ? minusPage() : e.shiftKey ? minusCurrentPage() : minusRow(e);
+        e.ctrlKey ? minusPage() : e.shiftKey ? minusCurrentPage() : minusRow();
     });
 
     $(document).on('click', '.edit', () => {
         displayTableSpan(true);
     });
 
+    function checkTestCaseData() {
+        return !(testCaseName === '' || projectName === '' || pageUrl.length < pageCount);
+    }
+
+    function testCaseDetail() {
+        console.log(testCaseName);
+        console.log(projectName);
+        console.log(pageUrl);
+    }
+
     $(document).on('click', '.save', async () => {
-        // if (testCase.testCaseName + testCase.projectName === '') {
-        //     popupModelItem();
-        // }
+        testCaseDetail();
+        if (!checkTestCaseData()) {
+            popupTestCaseInformation();
+            $('#pop-test-case-name').val(testCaseName || '');
+            let $popUrl = $('.pop-url');
+            $popUrl.each((i, e) => {
+                e.value = pageUrl[i] || '';
+            });
+            return;
+        }
         displayTableSpan(false);
         let data = await sendJsonObjectArray();
         if (data === 'ok') {
